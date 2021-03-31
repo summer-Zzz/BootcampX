462f76a9e5364b15cddd3b7d12897990305240a8
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-31 23:47:38 +0000

    add Parameterized Query to students and teachers.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,15 @@ const pool = new Pool({
 
 const cohort = process.argv[2];
 const limit = process.argv[3];
-
-pool.query(`
+const values = [`%${cohort}%`, limit];
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${limit || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+pool.query(queryString, values);
 
 .then(res => {
   res.rows.forEach(user => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,19 @@ const pool = new Pool({
 });
 
 const cohort = process.argv[2];
+const values = [`%${cohort}%`];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohort}'
+WHERE cohorts.name = $1'
 ORDER BY teachers.name;
-`)
+`
+
+pool.query(queryString, values);
 
 .then(res => {
   res.rows.forEach(row => {
